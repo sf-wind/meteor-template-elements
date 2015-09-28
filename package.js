fd1c8e761c4a7f639447070682d4e94c8b0ed29a
sf-wind <sf_wind@hotmail.com>
2015-09-27 17:06:06 -0700

    upgrade to support Meteor 1.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'fsun:template-elements',
-  version: '0.1.4',
+  version: '0.1.6',
   // Brief, one-line summary of the package.
   summary: 'Common template elements to simplify view design. ',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.2.0.1');
   api.use('coffeescript');
   api.use("templating", "client");
   api.use("blaze", "client");
@@ -24,34 +24,34 @@ Package.onUse(function(api) {
   api.addFiles('exports.coffee');
   api.export("BElements");
 
-  api.addFiles('client/list/list.html');
-  api.addFiles('client/list/list.coffee');
-  api.addFiles('client/list/list.less');
+  api.addFiles('client/list/list.html', 'client');
+  api.addFiles('client/list/list.coffee', 'client');
+  api.addFiles('client/list/list.less', 'client');
 
-  api.addFiles('client/header/header.html');
-  api.addFiles('client/header/header.coffee');
-  api.addFiles('client/header/header.less');
+  api.addFiles('client/header/header.html', 'client');
+  api.addFiles('client/header/header.coffee', 'client');
+  api.addFiles('client/header/header.less', 'client');
 
-  api.addFiles('client/footer/footer.html');
-  api.addFiles('client/footer/footer.coffee');
-  api.addFiles('client/footer/footer.less');
+  api.addFiles('client/footer/footer.html', 'client');
+  api.addFiles('client/footer/footer.coffee', 'client');
+  api.addFiles('client/footer/footer.less', 'client');
 
-  api.addFiles('client/pagination/pagination.html');
-  api.addFiles('client/pagination/pagination.coffee');
-  api.addFiles('client/pagination/pagination.less');
+  api.addFiles('client/pagination/pagination.html', 'client');
+  api.addFiles('client/pagination/pagination.coffee', 'client');
+  api.addFiles('client/pagination/pagination.less', 'client');
 
-  api.addFiles('client/code/code.html');
-  api.addFiles('client/code/code.coffee');
-  api.addFiles('client/code/code.less');
+  api.addFiles('client/code/code.html', 'client');
+  api.addFiles('client/code/code.coffee', 'client');
+  api.addFiles('client/code/code.less', 'client');
 
-  api.addFiles('client/calendar/calendar.html');
-  api.addFiles('client/calendar/calendar.coffee');
-  api.addFiles('client/calendar/calendar.less');
+  api.addFiles('client/calendar/calendar.html', 'client');
+  api.addFiles('client/calendar/calendar.coffee', 'client');
+  api.addFiles('client/calendar/calendar.less', 'client');
 
 });
 
 Package.onTest(function(api) {
-  api.use("sanjo:jasmine@0.17.0")
+  api.use("sanjo:jasmine@0.18.0")
 
   api.use('coffeescript');
   api.use("templating", "client");
@@ -59,7 +59,7 @@ Package.onTest(function(api) {
   api.use("jquery", "client");
   api.use("fsun:template-elements")
 
-  api.addFiles('tests/client/code/footer-example.txt', 'server', {isAsset : true})
+  api.addAssets('tests/client/code/footer-example.txt', 'server')
   api.addFiles('tests/_.coffee', 'client');
   api.addFiles('tests/client/list/list.coffee', 'client');
   api.addFiles('tests/client/header/header.coffee', 'client');
